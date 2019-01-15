37029f048eee33826c3d171ccc953f480a4a8601
afterloe <605728727@qq.com>
2019-01-15 21:43:28 +0800

    feat: 添加文件下载功能

--- routers/FileSystem.go
+++ routers/FileSystem.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"../util"
 	"../services/borderSystem"
+	"os"
 )
 
 /**
@@ -13,39 +14,39 @@ import (
 func FsUpload(ctx *gin.Context) {
 	file, err := ctx.FormFile("file")
 	if nil != err {
-		ctx.JSON(http.StatusBadRequest, util.Fail(400, "file not found."))
+		ctx.SecureJSON(http.StatusBadRequest, util.Fail(400, "file not found."))
 		return
 	}
 	fs := borderSystem.Default(file.Filename, file.Header.Get("Content-Type"), file.Size)
 	object, err := fs.SaveToDB()
 	if nil != err {
-		ctx.JSON(http.StatusInternalServerError, util.Error(err))
+		ctx.SecureJSON(http.StatusInternalServerError, util.Error(err))
 		return
 	}
 	err = ctx.SaveUploadedFile(file, fs.GeneratorSavePath())
 	if nil != err {
 		fs.Del(true)
-		ctx.JSON(http.StatusInternalServerError, util.Fail(500, "io exception."))
+		ctx.SecureJSON(http.StatusInternalServerError, util.Fail(500, "io exception."))
 		return
 	}
-	ctx.JSON(http.StatusOK, util.Success(object))
+	ctx.SecureJSON(http.StatusOK, util.Success(object))
 }
 
 func FsListAll(ctx *gin.Context) {
 	reply := borderSystem.GetAll(pageCondition(ctx))
-	ctx.JSON(http.StatusOK, util.Success(reply))
+	ctx.SecureJSON(http.StatusOK, util.Success(reply))
 }
 
 func FsList(ctx *gin.Context) {
 	reply := borderSystem.GetList(pageCondition(ctx))
-	ctx.JSON(http.StatusOK, util.Success(reply))
+	ctx.SecureJSON(http.StatusOK, util.Success(reply))
 }
 
 func FsDel(ctx *gin.Context) {
 	id := ctx.DefaultQuery("id", "")
 	f := ctx.DefaultQuery("f", "")
 	if "" == id {
-		ctx.JSON(http.StatusBadRequest, util.Fail(400, "lack param -> id"))
+		ctx.SecureJSON(http.StatusBadRequest, util.Fail(400, "lack param -> id"))
 		return
 	}
 	var (
@@ -57,22 +58,44 @@ func FsDel(ctx *gin.Context) {
 		err = borderSystem.Del(id, true)
 	}
 	if nil != err {
-		ctx.JSON(http.StatusInternalServerError, util.Error(err))
+		ctx.SecureJSON(http.StatusInternalServerError, util.Error(err))
 		return
 	}
-	ctx.JSON(http.StatusOK, util.Success("done"))
+	ctx.SecureJSON(http.StatusOK, util.Success("done"))
 }
 
 func FsFindOne(ctx *gin.Context) {
 	key := ctx.Param("key")
 	if "" == key {
-		ctx.JSON(http.StatusBadRequest, util.Fail(400, "key not found"))
+		ctx.SecureJSON(http.StatusBadRequest, util.Fail(400, "key not found"))
 		return
 	}
 	reply, err := borderSystem.GetOne(key)
 	if nil != err {
-		ctx.JSON(http.StatusInternalServerError, util.Error(err))
+		ctx.SecureJSON(http.StatusInternalServerError, util.Error(err))
 		return
 	}
-	ctx.JSON(http.StatusOK, util.Success(*reply))
+	ctx.SecureJSON(http.StatusOK, util.Success(*reply))
+}
+
+func FsDownload(ctx *gin.Context) {
+	key := ctx.Param("key")
+	if 32 > len(key) {
+		ctx.SecureJSON(http.StatusBadRequest, util.Fail(400, "参数错误"))
+		return
+	}
+	file, err := borderSystem.GetOne(key)
+	if nil != err {
+		ctx.SecureJSON(http.StatusInternalServerError, util.Error(err))
+		return
+	}
+	stat, err := os.Stat(file.GeneratorSavePath())
+	if nil != err {
+		ctx.SecureJSON(http.StatusNotFound, util.Fail(404, "file is not found"))
+		return
+	}
+	ctx.Header("Content-Type", file.ContentType)
+	ctx.Header("Content-Disposition", "attachment;filename=" + string([]byte(file.Name)))
+	ctx.Header("Content-Length", string(stat.Size()))
+	ctx.File(file.GeneratorSavePath())
 }
